2b364949d088abccd6cbbd6b14968f39444bc3bd
Lucas Vienna <dev@lucasvienna.dev>
2022-10-31 14:18:19 +0100

    Initial release

---
+++ ClickCastingTab/Main.lua
@@ -0,0 +1,17 @@
+local addonName, addon = ...
+
+-- Globals
+local _G = _G
+local ClickCastingSkillLineTab = _G.ClickCastingSkillLineTab -- our frame name, see ClickCastingTab.xml
+
+-- Addon Core
+addon.eventFrame = CreateFrame("Frame", addonName .. "EventFrame", UIParent)
+addon.eventFrame:RegisterEvent("ADDON_LOADED")
+addon.eventFrame:SetScript("OnEvent", function (frame, event, ...)
+	if event == "ADDON_LOADED" then
+		if ... == addonName then
+			addon.eventFrame:UnregisterEvent("ADDON_LOADED")
+			ClickCastingSkillLineTab.tooltip = "Click Cast Bindings"
+		end
+	end
+end)
---
+++ README.md
@@ -0,0 +1,12 @@
+# ClickCastingTab
+> Adds a tab to the spellbook to quickly open the Click Casting window.
+
+Does what the title says. *Taintless, fast, and small!*
+
+Clicking the tab will open the Click Cast Bindings frame, and clicking it again will
+save the changes.
+
+## Support & Connect
+- GitHub: [https://github.com/lucasvienna](lucasvienna)
+- Twitter: [https://twitter.com/Avyiel7](@Avyiel7)
+- PayPal: [https://paypal.me/lvienna](https://paypal.me/lvienna)
