30c6e44d5dfa896f6658f2a090805e86ea4de0ba
Lucas Vienna <dev@lucasvienna.dev>
2023-11-08 12:12:28 +0100

    Improve IntelliSense types

--- ClickCastingTab/Main.lua
+++ ClickCastingTab/Main.lua
@@ -2,12 +2,13 @@ local addonName, addon = ...
 
 -- Globals
 local _G = _G
+---@class CheckButton
 local ClickCastingSkillLineTab = _G.ClickCastingSkillLineTab -- our frame name, see ClickCastingTab.xml
 
 -- Addon Core
 addon.eventFrame = CreateFrame("Frame", addonName .. "EventFrame", UIParent)
 addon.eventFrame:RegisterEvent("ADDON_LOADED")
-addon.eventFrame:SetScript("OnEvent", function (frame, event, ...)
+addon.eventFrame:SetScript("OnEvent", function(frame, event, ...)
 	if event == "ADDON_LOADED" then
 		if ... == addonName then
 			addon.eventFrame:UnregisterEvent("ADDON_LOADED")
